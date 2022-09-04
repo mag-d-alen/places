155720c9b177481ab1fffc37910e201530c0f3ed
Magdalena BenBassatLuszczynska <magdalena@a-i.com>
2022-09-04 11:48:30 +0300

    redux branch created

---
+++ src/functions/fetchData.ts
@@ -0,0 +1,8 @@
+export const fetchOneUser = async () => {
+  // try {
+  //   const user = await dispatch(fetchUserById(userId)).unwrap();
+  //   showToast("success", `Fetched ${user.name}`);
+  // } catch (err) {
+  //   showToast("error", `Fetch failed: ${err.message}`);
+  // }
+};
